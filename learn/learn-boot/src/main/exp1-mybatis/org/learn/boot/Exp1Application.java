0233b9da63b4266a9e9d96ac8a0575c0e809296a
fans <fans_2046@126.com>
2016-03-18 17:26:11 +0800

    velicoty　配置OK

--- learn/learn-boot/src/main/exp1-mybatis/org/learn/boot/Exp1Application.java
+++ learn/learn-boot/src/main/exp1-mybatis/org/learn/boot/Exp1Application.java
@@ -1,19 +1,22 @@
 package org.learn.boot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * spring boot 启动入口
  * @author fwg create by  2016年3月3日 下午2:59:21
  *
  */
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration  
 @EnableConfigurationProperties({JdbcConfig.class})
-
-//@Configuration  
+@ComponentScan
 //@EnableAutoConfiguration  
+
+ 
 public class Exp1Application {
 
     public static void main(String[] args) {
